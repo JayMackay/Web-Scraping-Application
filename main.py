c5406ec60ea2466b94c71d257fd95d7c45f4c647
JayMackay <jared.mackay1994@live.com>
2020-01-20 22:52:41 +0000

    Extract Embedded HTML, Store CSV Data

--- main.py
+++ main.py
@@ -15,3 +15,40 @@ page_soup = soup(page_html, "html.parser")
 
 #Find all division elements with the class object "item-container"
 containers = page_soup.findAll("div", {"class":"item-container"})
+
+#Initialize CSV file to store extracted data
+out_filename = "graphics_cards.csv"
+
+#Header of CSV file to be written
+headers = "brand, product_name, shipping \n"
+
+#Opens file, and writes headers
+f = open(out_filename, "w")
+f.write(headers)
+
+#For loops to grab attributes for each product
+for container in containers:
+   
+    #Find all hyperlink elements within the first division
+    make_rating_sp = container.div.select("a")
+
+    #Grab the title from the image title attribute
+    brand = make_rating_sp[0].img["title"].title()
+
+    #Grab the text within the second "a" tag from within the list of queries
+    product_name = container.div.select("a")[2].text
+
+    #Find all list elements with the class "price-ship"
+    #Clean the text of white space with strip()
+    #Clean the strip of "Shipping $" if it exists to just get number
+    shipping = container.findAll("li", {"class": "price-ship"})[0].text.strip().replace("$", "").replace(" Shipping", "")
+
+    #Prints the dataset to console
+    print("brand: " + brand + "\n")
+    print("product_name: " + product_name + "\n")
+    print("shipping: " + shipping + "\n")
+
+    #Writes the dataset to file
+    f.write(brand + ", " + product_name.replace(",", " ") + ", " + shipping + "\n")
+
+f.close() 
